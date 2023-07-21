9ff204172a2f029f9048705944dd5274da52a5ce
MariuszMalec <mario106c@gmail.com>
2023-07-21 11:34:34 +0200

    fix tests naming to add ext in vsc

--- test_Solution.py
+++ test_Solution.py
@@ -1,27 +1,27 @@
 import unittest
 import Solution
 
-class Testing(unittest.TestCase):
+class Test_SmallestPositiveInteger(unittest.TestCase):
 
     def test_solution_Return5(self):
         lista = [1, 3, 6, 4, 1, 2]
         solution = Solution.solution(lista)
-        self.assertEquals(solution,5)
+        self.assertEqual(solution,5)
 
     def test_solution_Return1(self):
         lista = [-1, -3]
         solution = Solution.solution(lista)
-        self.assertEquals(solution,1)
+        self.assertEqual(solution,1)
 
     def test_solution_Return4(self):
         lista = [1,2,3]
         solution = Solution.solution(lista)
-        self.assertEquals(solution,4)
+        self.assertEqual(solution,4)
 
     def test_solution_Return2(self):
         lista = [-1, -3, 0, 1, 3, 4, 6]
         solution = Solution.solution(lista)
-        self.assertEquals(solution,2)
+        self.assertEqual(solution,2)
 
 if __name__ == '__main__':
     unittest.main()
